f14e217de8a9a8035d25aac81713a0cdd11d2b35
jhonspillaca12@gmail.com <jhonspillaca12@gmail.com>
2021-04-09 10:51:14 -0500

    version 0.1.0

---
+++ view/header.php
@@ -0,0 +1,52 @@
+<div class="wrapper">
+    <nav>
+        <input type="checkbox" id="show-user">
+        <input type="checkbox" id="show-menu">
+        <label for="show-menu" class="menu-icon"><i class="fas fa-bars"></i></label>
+        <div class="content">
+            <div class="logo">
+                <a href="inicio">
+                <label for="">LOGO</label>
+                    <!-- <img src="./assets/img/logo.png" alt="LOGO"> -->
+                </a>
+            </div>
+
+            <ul class="links">
+                <li>
+                    <a href="foro"><i class="fas fa-globe"></i> Foro</a>
+                    <!-- <a class="desktop-link">COMUNIDAD <i class="fas fa-caret-down"></i></a> -->
+                    <!-- <input type="checkbox" id="show-features">
+                    <label for="show-features">COMUNIDAD <i class="fas fa-caret-down"></i></label>
+                    <ul>
+                        <li><i class="fas fa-caret-right"></i><a href="#">submenu</a></li>
+                    </ul> -->
+                </li>
+
+                <li>
+                    <a href="comunidad"><i class="fas fa-users"></i> Comunidad</a>
+                    
+                </li>
+                <li><a href="ayuda"><i class="fas fa-question"></i> Ayuda </a>
+                
+
+                </li>
+                <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Busca tu libro/autor" aria-label="Search">
+    <button class="btn btn-outline-primary my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+  </form>
+            </ul>
+        </div>
+        <div class="logins">
+
+            <label for="show-user"><i class="principal fas fa-user"></i></label>
+
+            <div class="options searh-icon">
+                <a href="registro" class="btn-login">Registrate</a>
+                <a href="login" class="btn-login">Login</a>
+            </div>
+
+        </div>
+    </nav>
+</div>
+
+</header>
---
+++ view/inicio.php
@@ -0,0 +1,11 @@
+
+<h1>body book web</h1>
+<div class="input-group has-validation">
+  <div class="input-group-prepend">
+    <span class="input-group-text"><a href="#" class="btn btn-primary">Filtro</a></span>
+  </div>
+  <input type="text" class="form-control" required>
+  <div class="invalid-feedback">
+    Please choose a username.
+  </div>
+</div>
